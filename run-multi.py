9b0718c50a0a366082ca4bb3edb0dd18be69da73
ioverfl0w <ioverfl0w@users.noreply.github.com>
2017-08-08 18:58:55 -0400

    Added Freenode to the multi client example.

--- run-multi.py
+++ run-multi.py
@@ -2,7 +2,7 @@ from lib import Engine
 from mods import Hilight
 from mods import Stats
 
-# Example configuration for multiple clients under a single program
+# Example configuration for multiple clients on multiple networks under a single program
 
 # Engine creation
 # Debug mode is set when any value but 0 is declared in args
@@ -18,6 +18,12 @@ clients = {'clients': [Engine.Profile('token', rizon),
                 Engine.Profile('tiken', rizon),
                 Engine.Profile('teken', rizon) ] }
 
+# Demonstrate a 4th connection to a separate network, using the single
+# client format rather than the multi client Json
+freenode = Engine.Profile('mate-pyirc', Engine.Network('Freenode', 'irc.freenode.net', 6697, True))
+freenode.ajoin = ['##mate-testing']
+
+
 # Client autojoin channels
 clients['ajoin'] = ['#apollo']
 
@@ -25,7 +31,8 @@ clients['ajoin'] = ['#apollo']
 clients['umodes'] = "+p"
 
 # Insert our new Clients into engine
-engine.addClients(clients)
+engine.addClients(clients) #insert out list of Rizon clients
+engine.addClient(freenode) # insert our single Freenode client
 
 # Load our modules
 engine.event.loadMod(Hilight.Hilight('paste.ee-api-key-here'))
